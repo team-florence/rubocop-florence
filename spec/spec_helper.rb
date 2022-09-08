0002197a49d5c960701acc93d2a3f82ee0be0593
Sam Jenkins <dustpan.umpires0y@icloud.com>
2022-09-08 09:26:04 +0100

    rspec does not fail for no examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@ RSpec.configure do |config|
   config.disable_monkey_patching!
   config.raise_errors_for_deprecations!
   config.raise_on_warning = true
-  config.fail_if_no_examples = true
+  # config.fail_if_no_examples = true
 
   config.order = :random
   Kernel.srand config.seed
